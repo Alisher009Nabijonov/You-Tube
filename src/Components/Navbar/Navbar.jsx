68533aabb37d27f85a7aecc4399b51b7ffbe7444
Alisher <Alisher.email@example.com>
2025-04-07 21:20:38 +0300

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Navbar.css";
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import Logo from "../../assets/youtube.svg";
+import Search from "../../assets/search.svg";
+import Upload from "../../assets/upload.svg";
+import more from "../../assets/more.svg";
+import notification from "../../assets/notification.svg";
+import profil from "../../assets/profil.jpg";
+const Navbar = () => {
+  return (
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <HiBars3BottomLeft />
+        <img className="logo" src={Logo} alt="" />
+      </div>
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <img src={Search} alt="" />
+        </div>
+      </div>
+      <div className="nav-right flex-div">
+        <img src={Upload} alt="" />
+        <img src={more} alt="" />
+        <img src={notification} alt="" />
+        <img src={profil} className="user-icon" alt="" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div>31:42 ga keldim</div>
+  )
+}
+
+export default Sidebar
